b81cc3d427a5234d417e3ebd03d115d1c4ddace0
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2021-02-20 19:59:21 +0200

    Add controls

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,23 @@ interface SimulationConfig extends DrawingConfig {
     windowHeight?: number;
 }
 
+const createRandomizedBoidProperties = (baseProperties: BoidProperties, world: World, pad: number) => {
+    return {
+        ...baseProperties,
+        pos: randomPosition({ maxX: world.width - pad, minX: pad, maxY: world.height - pad, minY: pad }),
+        color: randomColor(240, 'lightness', { minLightness: 20, maxLightness: 70 }),
+        direction: randomDirection(),
+    };
+}
 
 const createWorld = (config: SimulationConfig) => {
     const world = new World(config.windowWidth, config.windowHeight);
     const n = config.amountOfBoids;
     const pad = 20;
     for (let i=0; i < n; i++) {
-        world.addBoid({
-            ...(config.boidProperties|| {}),
-            pos: randomPosition({ maxX: world.width - pad, minX: pad, maxY: world.height - pad, minY: pad }),
-            color: randomColor(240, 'lightness', { minLightness: 20, maxLightness: 70 }),
-            direction: randomDirection(),
-        });
+        world.addBoid(
+            createRandomizedBoidProperties(config.boidProperties, world, pad)
+        );
     }
     return world;
 }
@@ -60,4 +65,65 @@ window.onload = window.onresize = () => {
     world.height = canvas.height = canvas.offsetHeight;
 };
 
+
+// controls
+const boidAmountSlider: HTMLInputElement = document.getElementById('boid-amount-slider') as HTMLInputElement;
+if (boidAmountSlider !== null) {
+    boidAmountSlider.oninput = (event: Event) => {
+        const value = (event.target as HTMLInputElement).valueAsNumber;
+        const diff = world.amountOfBoids() - value;
+        for (let i=0; i<Math.abs(diff); i++) {
+            if (diff > 0) {
+                world.removeBoid();
+            } else {
+                const properties = createRandomizedBoidProperties(config.boidProperties, world, 20); 
+                world.addBoid(properties);
+            }
+        }
+    };
+    boidAmountSlider.value = '' + config.amountOfBoids;
+} else {
+    console.warn("unable to get boid amount slider");
+}
+
+const boidSizeSlider: HTMLInputElement = document.getElementById('boid-size-slider') as HTMLInputElement;
+if (boidSizeSlider !== null) {
+    boidSizeSlider.oninput = (event: Event) => {
+        const value = (event.target as HTMLInputElement).valueAsNumber;
+        config.boidProperties.size = value;
+        world.changeBoidSize(value);
+    };
+    boidSizeSlider.value = '' + (config.boidProperties.size || 5);
+} else {
+    console.warn("unable to get boid size slider");
+}
+
+const showCoM = document.getElementById('show-center-of-mass');
+if (showCoM !== null) {
+    showCoM.onchange = (event: Event) => {
+        config.showCenterOfMass = (event.target as HTMLInputElement).checked;
+    };
+} else {
+    console.warn('unable to get input "show center of mass"');
+}
+
+const showBoidVision = document.getElementById('show-vision-of-boids');
+if (showBoidVision !== null) {
+    showBoidVision.onchange = (event: Event) => {
+        config.showVision = (event.target as HTMLInputElement).checked;
+    };
+} else {
+    console.warn('unable to get input show-boid-vision');
+}
+
+const showGroups = document.getElementById('show-groups');
+if (showGroups !== null) {
+    showGroups.onchange = (event: Event) => {
+        config.showGroup = (event.target as HTMLInputElement).checked;
+    };
+} else {
+    console.warn('unable to get show-groups');
+}
+
+// Start
 play();
